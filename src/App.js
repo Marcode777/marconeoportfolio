c7335b2cc4c2caf773290899d39e21655982f359
Marcode777 <marcosebello7@gmail.com>
2016-10-30 15:03:05 -0400

    added second section

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div className="first">
+          <p>First Section</p>
+        </div>
+         <div className="second">
+          <p>Second Section</p>
         </div>
 
 
