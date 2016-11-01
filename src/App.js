78744c60bb41d07a07b11c23f6efbb52d9df0f66
Marcode777 <marcosebello7@gmail.com>
2016-11-01 07:34:47 -0400

    modified section after header

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to My Portfolio Page!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  
         <div className="first">
           <p>First Section</p>
         </div>
