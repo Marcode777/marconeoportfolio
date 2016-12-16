5be39f854f39b0d97a6ee9e7b4915e5ebdb97cec
Marcode777 <marcosebello7@gmail.com>
2016-12-16 11:47:40 -0500

    setup webpack.config.js, src folder, components folder, index.js file, app.js file, index.html file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log("yeah, this works"); //this shows that index.js is working
+let message = 'HELLO FROM ENTRY!!!'; // this shows that both index.js and es6 is working as well (also, just changing any part of these messages and then after saving, if the changes update, then that's confirmation that live-reloading is working as well)
+console.log(message); // this shows that both index.js and es6 is working as well also
+
+import React from 'react';
+import {render} from 'react-dom';
+import App from 'components/app';
+
+
+render(<App/>, document.getElementById('app')); // this is the id where the react app will actually be displayed into the html page
