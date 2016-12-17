85d1271db3812332c78beae9f10640e71154a16e
Marcode777 <marcosebello7@gmail.com>
2016-12-17 06:27:45 -0500

    modified src/components/display.js

--- src/components/display.js
+++ src/components/display.js
@@ -5,6 +5,9 @@ export default class Display extends React.Component{
     return(
       <div>
         <h2>display.js</h2>
+        <div>
+          <h3>display.js2</h3>
+        </div>
       </div>
       );
   }
