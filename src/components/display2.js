0eca39f65130a199352fdb9561505d6af6598c74
Marcode777 <marcosebello7@gmail.com>
2016-12-18 09:03:16 -0500

    modified display2.js

---
+++ src/components/display2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Display2 extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>display2</h2>
+        <p>paragraph</p>
+      </div>
+      );
+  }
+}
