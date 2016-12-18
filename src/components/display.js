cad5d51dddd77e04680160826aad702ea16b4896
Marcode777 <marcosebello7@gmail.com>
2016-12-18 18:01:12 -0500

    improved div structure

--- src/components/display.js
+++ src/components/display.js
@@ -16,10 +16,10 @@ export default class Display extends React.Component{
     return(
       <div>
         <div style={Style1}>
-          <h2>Display.js</h2>
+          <h2>Display1</h2>
         </div>
         <div style={Style2}>
-          <h3>Display.js</h3>
+          <h3>Display2</h3>
         </div>
       </div>
 
